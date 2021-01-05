9b008d38fa4e3b1e4d1d79cba18060c85cc02c2a
Scarecrow <Scarecrow@LAPTOP-HE4ANLKG>
2021-01-05 11:17:36 +0100

    La till checkbox

--- grupp19_lab2/Hjälp.Designer.cs
+++ grupp19_lab2/Hjälp.Designer.cs
@@ -39,6 +39,7 @@ namespace grupp19_lab2
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -127,11 +128,22 @@ namespace grupp19_lab2
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(172, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Har johannes sett min ändring?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Hjälp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 537);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
@@ -163,5 +175,6 @@ namespace grupp19_lab2
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
