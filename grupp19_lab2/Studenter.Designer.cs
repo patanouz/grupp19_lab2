46d1420e2231ace49969782a63b1e35b99b7c302
johannes <johannes@DESKTOP-3359OV7>
2021-01-05 10:58:42 +0100

    lagt till knapp2

--- grupp19_lab2/Studenter.Designer.cs
+++ grupp19_lab2/Studenter.Designer.cs
@@ -30,26 +30,38 @@ namespace grupp19_lab2
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(410, 233);
+            this.button1.Location = new System.Drawing.Point(62, 49);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 130);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 190);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Studenter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1437, 991);
+            this.ClientSize = new System.Drawing.Size(1120, 840);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Studenter";
             this.Text = "Studenter";
             this.Load += new System.EventHandler(this.Studenter_Load);
@@ -60,5 +72,6 @@ namespace grupp19_lab2
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
