9e7f24ed4170c0f8b1f31f29b5b7fd28c2fd5f87
Scarecrow <Scarecrow@LAPTOP-HE4ANLKG>
2021-01-05 11:08:19 +0100

    Fixar studenter

--- grupp19_lab2/Studenter.Designer.cs
+++ grupp19_lab2/Studenter.Designer.cs
@@ -35,32 +35,33 @@ namespace grupp19_lab2
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(62, 49);
+            this.button1.Location = new System.Drawing.Point(34, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 130);
+            this.button1.Size = new System.Drawing.Size(88, 70);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(387, 284);
+            this.button2.Location = new System.Drawing.Point(211, 154);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(229, 190);
+            this.button2.Size = new System.Drawing.Size(125, 103);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Studenter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(784, 537);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Studenter";
             this.Text = "Studenter";
             this.Load += new System.EventHandler(this.Studenter_Load);
