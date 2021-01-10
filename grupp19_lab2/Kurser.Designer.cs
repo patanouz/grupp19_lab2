b69c80197b87879310717169a5e8e158147c272b
Johan Christersson <johan.christersson@live.se>
2021-01-10 15:38:46 +0100

    Fixat kurser

--- grupp19_lab2/Kurser.Designer.cs
+++ grupp19_lab2/Kurser.Designer.cs
@@ -29,28 +29,24 @@ namespace grupp19_lab2
         /// </summary>
         private void InitializeComponent()
         {
-            this.kursListBox = new System.Windows.Forms.ListBox();
             this.lärarlagComboBox = new System.Windows.Forms.ComboBox();
             this.lärarlagLabel = new System.Windows.Forms.Label();
             this.kursmomentTextBox = new System.Windows.Forms.TextBox();
             this.kursmomentLabel = new System.Windows.Forms.Label();
             this.kursNamnTextBox = new System.Windows.Forms.TextBox();
             this.kursNamnLabel = new System.Windows.Forms.Label();
+            this.kursmomentListBox = new System.Windows.Forms.ListBox();
+            this.addKursmomentButton = new System.Windows.Forms.Button();
+            this.kursmomentGroupBox = new System.Windows.Forms.GroupBox();
+            this.addKursButton = new System.Windows.Forms.Button();
+            this.availableStudentsListBox = new System.Windows.Forms.ListBox();
+            this.addedStudentsListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // kursListBox
-            // 
-            this.kursListBox.FormattingEnabled = true;
-            this.kursListBox.ItemHeight = 20;
-            this.kursListBox.Location = new System.Drawing.Point(786, 100);
-            this.kursListBox.Name = "kursListBox";
-            this.kursListBox.Size = new System.Drawing.Size(285, 364);
-            this.kursListBox.TabIndex = 0;
             // 
             // lärarlagComboBox
             // 
             this.lärarlagComboBox.FormattingEnabled = true;
-            this.lärarlagComboBox.Location = new System.Drawing.Point(118, 552);
+            this.lärarlagComboBox.Location = new System.Drawing.Point(95, 219);
             this.lärarlagComboBox.Name = "lärarlagComboBox";
             this.lärarlagComboBox.Size = new System.Drawing.Size(137, 28);
             this.lärarlagComboBox.TabIndex = 1;
@@ -58,7 +54,7 @@ namespace grupp19_lab2
             // lärarlagLabel
             // 
             this.lärarlagLabel.AutoSize = true;
-            this.lärarlagLabel.Location = new System.Drawing.Point(117, 526);
+            this.lärarlagLabel.Location = new System.Drawing.Point(95, 193);
             this.lärarlagLabel.Name = "lärarlagLabel";
             this.lärarlagLabel.Size = new System.Drawing.Size(67, 20);
             this.lärarlagLabel.TabIndex = 2;
@@ -66,7 +62,7 @@ namespace grupp19_lab2
             // 
             // kursmomentTextBox
             // 
-            this.kursmomentTextBox.Location = new System.Drawing.Point(124, 642);
+            this.kursmomentTextBox.Location = new System.Drawing.Point(95, 520);
             this.kursmomentTextBox.Name = "kursmomentTextBox";
             this.kursmomentTextBox.Size = new System.Drawing.Size(160, 26);
             this.kursmomentTextBox.TabIndex = 3;
@@ -74,7 +70,7 @@ namespace grupp19_lab2
             // kursmomentLabel
             // 
             this.kursmomentLabel.AutoSize = true;
-            this.kursmomentLabel.Location = new System.Drawing.Point(121, 616);
+            this.kursmomentLabel.Location = new System.Drawing.Point(91, 496);
             this.kursmomentLabel.Name = "kursmomentLabel";
             this.kursmomentLabel.Size = new System.Drawing.Size(99, 20);
             this.kursmomentLabel.TabIndex = 4;
@@ -82,7 +78,7 @@ namespace grupp19_lab2
             // 
             // kursNamnTextBox
             // 
-            this.kursNamnTextBox.Location = new System.Drawing.Point(118, 157);
+            this.kursNamnTextBox.Location = new System.Drawing.Point(95, 119);
             this.kursNamnTextBox.Name = "kursNamnTextBox";
             this.kursNamnTextBox.Size = new System.Drawing.Size(181, 26);
             this.kursNamnTextBox.TabIndex = 5;
@@ -90,24 +86,85 @@ namespace grupp19_lab2
             // kursNamnLabel
             // 
             this.kursNamnLabel.AutoSize = true;
-            this.kursNamnLabel.Location = new System.Drawing.Point(118, 131);
+            this.kursNamnLabel.Location = new System.Drawing.Point(96, 95);
             this.kursNamnLabel.Name = "kursNamnLabel";
             this.kursNamnLabel.Size = new System.Drawing.Size(81, 20);
             this.kursNamnLabel.TabIndex = 6;
             this.kursNamnLabel.Text = "Kursnamn";
+            // 
+            // kursmomentListBox
+            // 
+            this.kursmomentListBox.FormattingEnabled = true;
+            this.kursmomentListBox.ItemHeight = 20;
+            this.kursmomentListBox.Location = new System.Drawing.Point(295, 468);
+            this.kursmomentListBox.Name = "kursmomentListBox";
+            this.kursmomentListBox.Size = new System.Drawing.Size(192, 204);
+            this.kursmomentListBox.TabIndex = 7;
+            // 
+            // addKursmomentButton
+            // 
+            this.addKursmomentButton.Location = new System.Drawing.Point(100, 565);
+            this.addKursmomentButton.Name = "addKursmomentButton";
+            this.addKursmomentButton.Size = new System.Drawing.Size(120, 53);
+            this.addKursmomentButton.TabIndex = 8;
+            this.addKursmomentButton.Text = "Lägg till";
+            this.addKursmomentButton.UseVisualStyleBackColor = true;
+            this.addKursmomentButton.Click += new System.EventHandler(this.addKursmomentButton_Click);
+            // 
+            // kursmomentGroupBox
+            // 
+            this.kursmomentGroupBox.Location = new System.Drawing.Point(84, 436);
+            this.kursmomentGroupBox.Name = "kursmomentGroupBox";
+            this.kursmomentGroupBox.Size = new System.Drawing.Size(454, 292);
+            this.kursmomentGroupBox.TabIndex = 9;
+            this.kursmomentGroupBox.TabStop = false;
+            this.kursmomentGroupBox.Text = "Kursmoment";
+            // 
+            // addKursButton
+            // 
+            this.addKursButton.Location = new System.Drawing.Point(736, 675);
+            this.addKursButton.Name = "addKursButton";
+            this.addKursButton.Size = new System.Drawing.Size(120, 53);
+            this.addKursButton.TabIndex = 10;
+            this.addKursButton.Text = "Lägg till";
+            this.addKursButton.UseVisualStyleBackColor = true;
+            this.addKursButton.Click += new System.EventHandler(this.addKursButton_Click);
+            // 
+            // availableStudentsListBox
+            // 
+            this.availableStudentsListBox.FormattingEnabled = true;
+            this.availableStudentsListBox.ItemHeight = 20;
+            this.availableStudentsListBox.Location = new System.Drawing.Point(598, 68);
+            this.availableStudentsListBox.Name = "availableStudentsListBox";
+            this.availableStudentsListBox.Size = new System.Drawing.Size(223, 424);
+            this.availableStudentsListBox.TabIndex = 11;
+            // 
+            // addedStudentsListBox
+            // 
+            this.addedStudentsListBox.FormattingEnabled = true;
+            this.addedStudentsListBox.ItemHeight = 20;
+            this.addedStudentsListBox.Location = new System.Drawing.Point(914, 68);
+            this.addedStudentsListBox.Name = "addedStudentsListBox";
+            this.addedStudentsListBox.Size = new System.Drawing.Size(223, 424);
+            this.addedStudentsListBox.TabIndex = 12;
             // 
             // Kurser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1176, 826);
+            this.Controls.Add(this.addedStudentsListBox);
+            this.Controls.Add(this.availableStudentsListBox);
+            this.Controls.Add(this.addKursButton);
+            this.Controls.Add(this.addKursmomentButton);
+            this.Controls.Add(this.kursmomentListBox);
             this.Controls.Add(this.kursNamnLabel);
             this.Controls.Add(this.kursNamnTextBox);
             this.Controls.Add(this.kursmomentLabel);
             this.Controls.Add(this.kursmomentTextBox);
             this.Controls.Add(this.lärarlagLabel);
             this.Controls.Add(this.lärarlagComboBox);
-            this.Controls.Add(this.kursListBox);
+            this.Controls.Add(this.kursmomentGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Kurser";
@@ -119,13 +176,17 @@ namespace grupp19_lab2
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox kursListBox;
         private System.Windows.Forms.ComboBox lärarlagComboBox;
         private System.Windows.Forms.Label lärarlagLabel;
         private System.Windows.Forms.TextBox kursmomentTextBox;
         private System.Windows.Forms.Label kursmomentLabel;
         private System.Windows.Forms.TextBox kursNamnTextBox;
         private System.Windows.Forms.Label kursNamnLabel;
+        private System.Windows.Forms.ListBox kursmomentListBox;
+        private System.Windows.Forms.Button addKursmomentButton;
+        private System.Windows.Forms.GroupBox kursmomentGroupBox;
+        private System.Windows.Forms.Button addKursButton;
+        private System.Windows.Forms.ListBox availableStudentsListBox;
+        private System.Windows.Forms.ListBox addedStudentsListBox;
     }
 }
--- grupp19_lab2/Kurser.cs
+++ grupp19_lab2/Kurser.cs
@@ -16,15 +16,17 @@ namespace grupp19_lab2
         Form1 form1;
         List<Kurs> kursLista = new List<Kurs>();
         List<Student> studentLista = new List<Student>();
-        List<LärarLag> lararlag = new List<LärarLag>();
+        List<LärarLag> lararlagLista = new List<LärarLag>();
+        List<Kursmoment> kursmomentLista = new List<Kursmoment>();
+        List<Student> addedStudents = new List<Student>();
 
         public Kurser(Form1 form1)
         {
             this.form1 = form1;
             InitializeComponent();
             StartData();
-            UppdateraKursListBox();
-
+            UppdateraLärarlagComboBox();
+            UppdateraStudenterListBox();
         }
 
         private void Kurser_Load(object sender, EventArgs e)
@@ -32,21 +34,31 @@ namespace grupp19_lab2
 
         }
 
-        private void UppdateraKursListBox() // Metod för att uppdatera listan med studenter vi har i lager.
+
+        private void UppdateraLärarlagComboBox()
         {
-            kursListBox.Items.Clear();
-            foreach (Kurs item in kursLista)
+            lärarlagComboBox.Items.Clear();
+            foreach (LärarLag ll in lararlagLista)
             {
-                kursListBox.Items.Add(item.HämtaNamn());
+                lärarlagComboBox.Items.Add(ll.Hämtanamn());
             }
         }
 
-        private void UppdateraLärarlagComboBoc()
+        private void UppdateraStudenterListBox()
         {
-            lärarlagComboBox.Items.Clear();
-            foreach (LärarLag ll in lararlag)
+            availableStudentsListBox.Items.Clear();
+            foreach (Student s in studentLista)
             {
-               // lärarlagComboBox.Items.Add(ll.)
+                availableStudentsListBox.Items.Add(s.HämtaNamn());
+            }
+        }
+
+        private void UppdateraAddedStudentsListBox()
+        {
+            addedStudentsListBox.Items.Clear();
+            foreach (Student s in addedStudents)
+            {
+                addedStudentsListBox.Items.Add(s.HämtaNamn());
             }
         }
 
@@ -55,6 +67,25 @@ namespace grupp19_lab2
             kursLista.Add(new Kurs("Roliga kursen", 1));
             kursLista.Add(new Kurs("OOAD", 2));
             kursLista.Add(new Kurs("Dataaa", 3));
+            lararlagLista.Add(new LärarLag("Lag 1"));
+            lararlagLista.Add(new LärarLag("B-laget"));
+            studentLista.Add(new Student("Rune", "Holta", "721108-5588", 1));
+            studentLista.Add(new Student("Brynolf", "Håkansson", "821108-4535", 2));
+            studentLista.Add(new Student("Jovarn", "Christersson", "951218-5682", 3));
+            studentLista.Add(new Student("Conny", "Oskarsson", "820530-5678", 4));
+        }
+
+        private void addKursmomentButton_Click(object sender, EventArgs e)
+        {
+            kursmomentListBox.Items.Add(kursmomentTextBox.Text);
+            kursmomentTextBox.Clear();
+        }
+
+        private void addKursButton_Click(object sender, EventArgs e)
+        {
+            Kurs k = new Kurs(kursNamnTextBox.Text, 1);
+            k.LäggTillStudent(addedStudents);
+
         }
     }
 }
